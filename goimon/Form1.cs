b1f046c8e2fe7de017f1cea4573a12a908a2d084
Hoang Minh <hoangminh1006@yahoo.com.vn>
2018-06-05 11:05:49 +0700

    giao dien chinh

--- goimon/Form1.cs
+++ goimon/Form1.cs
@@ -7,14 +7,89 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WindowsFormsApp1.Model;
 
-namespace goimon
+namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            View.Add frm = new View.Add();
+            frm.ShowDialog();
+            load_form();
+        }
+
+        public void load_form()
+        {
+            quanlygoimonEntities db = new quanlygoimonEntities();
+            var ra = db.Menu;
+            var rb = db.Table;
+            dataGridView1.DataSource = (from a in ra join b in rb on a.ID_Table equals b.ID select new { b.TableName, a.Food, a.QuantityofFood, a.PriceofFood, a.Drink, a.QuantityofDrink, a.PriceofDrink, a.Total, a.ID }).ToList();
+            //dataGridView1.Columns[0].Visible=  false;
+            //dataGridView1.Columns[9].Visible = false;
+            dataGridView1.Columns[8].Visible = false;
+
+            dataGridView1.Columns[0].HeaderText = "Bàn";
+            dataGridView1.Columns[1].HeaderText = "Món ăn";
+            dataGridView1.Columns[2].HeaderText = "Số lượng";
+            dataGridView1.Columns[3].HeaderText = "Giá";
+            dataGridView1.Columns[4].HeaderText = "Đồ uống";
+            dataGridView1.Columns[5].HeaderText = "Số lượng";
+            dataGridView1.Columns[6].HeaderText = "Giá";
+            dataGridView1.Columns[7].HeaderText = "Tổng cộng";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            load_form();
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            var id = (int)dataGridView1.SelectedRows[0].Cells[8].Value;
+            View.Edit frm = new View.Edit(id);
+            frm.ShowDialog();
+            load_form();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count == 1)
+            {
+                var id = (int)dataGridView1.SelectedRows[0].Cells[8].Value;
+                var db = new quanlygoimonEntities();
+                var menu = db.Menu.Find(id);
+                db.Menu.Remove(menu);
+                db.SaveChanges();
+                load_form();
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
